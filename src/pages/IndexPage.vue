430f5ad18e50f14aa2c5bd662e6c906130b7b174
arjun-dev <arjuna1@protonmail.com>
2026-01-24 11:47:58 -0600

    Implemented folder-list views

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -38,6 +38,18 @@
         </div>
         <div class="col-auto">
           <q-btn dense flat icon="refresh" :loading="loading" @click="refresh" />
+          <q-btn
+            class="q-ml-xs"
+            dense
+            flat
+            :icon="viewMode === 'folder' ? 'view_list' : 'grid_view'"
+            :disable="loading"
+            @click="toggleViewMode"
+          >
+            <q-tooltip>
+              Switch to {{ viewMode === 'folder' ? 'list' : 'folder' }} view
+            </q-tooltip>
+          </q-btn>
         </div>
       </div>
 
@@ -45,7 +57,7 @@
         {{ error }}
       </q-banner>
 
-      <q-card bordered>
+      <q-card v-if="viewMode === 'list'" bordered>
         <q-list separator>
           <q-item v-for="entry in entries" :key="entry.path" clickable @click="openEntry(entry)">
             <q-item-section avatar>
@@ -64,6 +76,30 @@
           </q-item>
         </q-list>
       </q-card>
+
+      <div v-else class="row q-col-gutter-sm">
+        <div v-for="entry in entries" :key="entry.path" class="col-6 col-sm-4 col-md-3 col-lg-2">
+          <q-card
+            bordered
+            class="full-height"
+            :class="entry.type === 'directory' ? 'cursor-pointer' : 'cursor-default'"
+            @click="openEntry(entry)"
+          >
+            <q-card-section class="q-pa-sm">
+              <div class="column items-center q-gutter-xs">
+                <q-icon :name="entry.type === 'directory' ? 'folder' : 'description'" size="34px" />
+                <div class="text-caption text-center ellipsis full-width">
+                  {{ entry.name }}
+                </div>
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
+
+        <div v-if="!loading && entries.length === 0" class="col-12">
+          <q-banner class="bg-grey-2 text-black" rounded>No entries</q-banner>
+        </div>
+      </div>
 
       <q-page-sticky position="bottom-right" :offset="[18, 18]">
         <q-btn
@@ -99,6 +135,7 @@ const pathInput = ref('')
 const entries = ref([])
 const loading = ref(false)
 const error = ref('')
+const viewMode = ref('folder')
 const creatingStructure = ref(false)
 const createResult = ref('')
 
@@ -127,6 +164,10 @@ async function loadDirectory (dirPath) {
 function openEntry (entry) {
   if (loading.value) return
   if (entry.type === 'directory') loadDirectory(entry.path)
+}
+
+function toggleViewMode () {
+  viewMode.value = viewMode.value === 'folder' ? 'list' : 'folder'
 }
 
 async function createStructure () {
