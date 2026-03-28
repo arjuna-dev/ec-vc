ef0019a71486675d9263838a10e76bb607938514
ErikB10 <erik@b10.global>
2026-03-28 17:58:14 -0600

    Remove home workspace cockpit label

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,7 +2,6 @@
   <q-page class="home-dashboard q-pa-md">
     <section class="home-dashboard__intro">
       <div class="home-dashboard__intro-copy">
-        <div class="home-dashboard__eyebrow">Workspace cockpit</div>
         <h1 class="home-dashboard__title">Home</h1>
       </div>
 
