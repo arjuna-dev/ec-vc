a5af78092e77f166537c1e0718fa1ee0d285b645
ErikB10 <erik@b10.global>
2026-04-04 02:43:47 -0600

    Darken test shell selector arrow

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1945,10 +1945,10 @@ function goBack() {
   right: -4px;
   bottom: -2px;
   z-index: 2;
-  color: #ffffff;
+  color: #111111;
   font-size: 20px;
   line-height: 1;
-  text-shadow: 0 1px 2px rgba(0, 0, 0, 0.45);
+  text-shadow: 0 1px 2px rgba(255, 255, 255, 0.18);
   pointer-events: none;
 }
 
