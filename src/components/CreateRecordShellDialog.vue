bc185a97ca979fbf624fc3f2f59ab6421ec4b16e
ErikB10 <erik@b10.global>
2026-04-04 10:49:56 -0600

    Finish summary label text alignment

--- src/components/CreateRecordShellDialog.vue
+++ src/components/CreateRecordShellDialog.vue
@@ -1026,7 +1026,7 @@ onBeforeUnmount(() => {
 }
 
 .create-record-shell__field--wide .create-record-shell__field-copy {
-  padding-top: 10px;
+  padding-top: 13px;
 }
 
 .create-record-shell__field-label {
