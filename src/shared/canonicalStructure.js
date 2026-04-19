088c7f705263b0e94b7ec085dd35fd4bdc9c2247
ErikB10 <erik@b10.global>
2026-04-19 08:19:03 -0600

    Delete dead canonical and opportunity scripts

--- src/shared/canonicalStructure.js
+++
@@ -1,9 +0,0 @@
-import canonicalStructure from '../../docs/000-canonical-structure.json' with { type: 'json' }
-
-export const CANONICAL_STRUCTURE_PATH = 'docs/000-canonical-structure.json'
-
-export function loadCanonicalStructure() {
-  return canonicalStructure
-}
-
-export default canonicalStructure
